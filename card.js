390fe26ef44b21c5e660679a913186895a149ccd
miaklwalker <ouisihai@gmail.com>
2019-09-12 02:27:52 -0500

    Co-authored-by: DrTerryDactyl <DrTerryDactyl@gmail.com>

--- card.js
+++ card.js
@@ -1,14 +1,15 @@
-let cards = require('./cards')
+// Magic Hate Ball
 
+let dice = Math.floor(Math.random()*8)
+let output
 
-let card 
-
-
-
-
-
-
-
-
-
-console.log(card)
+if(dice===0){output = "Yes";}
+if (dice === 1) {output = "Hello";}
+if (dice === 2) {output = "Goodbye";}
+if (dice === 3) {output = "Fuck you ";}
+if (dice === 4) {output = "I Said Good Day Sir";}
+if (dice === 5) {output = "Congratulations you played yourself";}
+if (dice === 6) {output = "Poo";}
+if (dice === 7) {output = "Another one";}
+if (dice === 8) {output = "Shiiiiiiiiiiiiit";}
+console.log(output)
--- cards.js
+++ cards.js
@@ -1,99 +1,121 @@
  let cards = {
   "Blueberry White Crust": {
     name: "Blueberry White Crust",
+    price:5,
     attack: 0,
     defense: 0,
-    type: "",
+    type: "pie",
     flavorText: "this legendary pie is an engine of destruction"
   },
 
   "Rasperry Burnt Crust": {
     name: "Rasperry Burnt Crust",
+    price:5,
     attack: 0,
     defense: 0,
-    type: "",
+    type: "pie",
     flavorText: "his crust is burnt so you know he's evil"
   },
 
   "Puffin": { 
-      name: "Puffin", 
+      name: "Puffin",
+      cost: 100 ,
       attack: 0, 
       defense: 0, 
-      type: "",
-       flavorText: "i am not a bird!"
+      type: "PieGod",
+       flavorText: "i am not a bird!",
+       effect(){
+         console.log('You Win!!')
+       }
      },
   "Bad Body Odor Dude": {
     name: "Bad Body Odor Dude",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "His name is Terance"
+    type: "BM BO God",
+    flavorText: "His name is Terance",
+    effect(){},
   },
   "Inupiepan": {
     name: "Inupiepan",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "Doggie Pie Pan Boi"
+    type: "As seen on Tv God",
+    flavorText: "Doggie Pie Pan Boi",
+    effect(){},
   },
   "The Pheonix": {
     name: "The Pheonix",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "Cuh Caw Muflapper i am a bird"
+    type: "Chicken Leg",
+    flavorText: "Cuh Caw Muflapper i am a bird",
+    effect(){},
   },
   "Jorge Busche The Press": {
     name: "Jorge Busche The Press",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "HE CAN BE PRESS RELATED"
+    type: "The Prez",
+    flavorText: "HE CAN BE PRESS RELATED",
+    effect(){},
+  },
+  "Magic Hate Ball":{
+    name: "Magic Hate ball",
+    attack: 0,
+    defense: 0,
+    type: "Clause",
+    flavorText: "",
+    effect(){},
   },
   "Tactical Rolex": {
     name: "Tactical Rolex",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Clause",
+    flavorText: "",
+    effect(){},
   },
   "Dynomite AfterParty": {
     name: "Dynomite AfterParty",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "singing backwards metal"
+    type: "Gotcha",
+    flavorText: "singing backwards metal",
+    effect(){},
+
   },
   "Pugeki": { 
       name: "Pugeki", 
       attack: 0,
        defense: 0, 
        type: "",
-        flavorText: ""
+        flavorText: "Clause",
+        effect(){},
+
      },
   "Basic Pie Soldier": {
     name: "Basic Pie Soldier",
+    price:4,
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Pie",
+    flavorText: " A basic Pie for a Basic Guy with a Basic Flavor Text",
+
+
   },
   "Army of Angry Dollars": {
     name: "Army of Angry Dollars",
     attack: 0,
     defense: 0,
     type: "",
-    flavorText: ""
+    flavorText: "",
+    effect(){
+      /*The more Money used to Buy the more powerfull they are  */
+    }
   },
   "Nail Gunners": {
     name: "Nail Gunners",
-    attack: 0,
-    defense: 0,
-    type: "",
-    flavorText: ""
-  },
-  "Bakery Bunch": {
-    name: "Bakery Bunch",
     attack: 0,
     defense: 0,
     type: "",
@@ -103,392 +125,404 @@
     name: "MR. Miasma",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Competitor",
     flavorText: ""
   },
   "NP": { 
       name: "NP", 
       attack: 0, 
       defense: 0, 
-      type: "", 
+      type: "Gotcha", 
       flavorText: "stamp of approval"
      },
   "But He's a Grappler": {
     name: "But He's a Grappler",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "he's unstopable mostly because he's a grappler"
   },
   "GutWrenchPickYaScoops": {
     name: "GutWrenchPickYaScoops",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "hit em' with one of these when they try and start shit"
   },
   "GlassCageOfEmotions": {
     name: "GlassCageOfEmotions",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
   "Launch The Missle Now": {
     name: "Launch The Missle Now ",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
   "Donut's Come to me": {
     name: "Donut's Come to me ",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: ""
   },
   "Piero Phorce": {
     name: "Piero Phorce",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
   "Tomato Bag": {
     name: "Tomato Bag",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "say one word"
   },
   "Black Custerd Pie Soldier": {
     name: "Black Cruster Pie Soldier",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "this is one Pie Solder that will never lose his Custerd"
+    type: "Pie",
+    flavorText: "this is one Pie Solder that will never lose his Custard"
   },
   "Nail Gun": {
     name: "Nail Gun",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "i think the nail gunners could find some use for this"
   },
   "Ghost of the Eskimo": {
     name: "Ghost of the Eskimo",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "don't worry he runs really slow"
+    type: "Hooligan",
+    flavorText: "don't worry he runs really slow",
+    effect(){
+      /*Can only attack every other turn */
+    }
   },
   "Breath of fresh air": {
     name: "Breath of fresh air",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "i mean, yeah. it does more than that right?"
   },
   "Sudden ClownPour": {
-    name: "Pie Defence Force",
+    name: "Sudden ClownPour",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Gotcha",
+    flavorText: "no one can hide",
+    effect(){
+      /*No one can attack for 3 turns , No one can hide */
+    }
   },
   "Pie Defence Force":{
     name: "Pie Defence Force",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Pie",
     flavorText: "it's different because it's a defence force"
   },
   "What the hell are you": {
     name: "What the hell are you",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Gotcha",
+    flavorText: "",
+    effect(){
+      /* They question the meaning of life an lose all attack points for one turn */
+    }
   },
   "Lucky Candy": {
     name: "Lucky Candy",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "never leave the cave without it "
   },
   "Shut your Pie hole": {
     name: "Shut your Pie hole",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "Someone Else Drew This"
   },
   "Get well soon card": {
     name: "Get well soon card",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "He doesn't really mean it"
   },
   "Grapeface": {
     name: "Grapeface ",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "he likes peanut butter"
+    type: "Hooligan",
+    flavorText: "he likes peanut butter, Good guy but got mixed in with the wrong crowd"
   },
   "Space Slam": {
     name: "Space Slam",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Hooligan",
     flavorText: "he's evil, but he's considerate of copyright laws"
   },
   "Bananaman":{
     name: "Bananaman",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Hooligan",
     flavorText: "EVERYBODY LOVES ME!"
   },
   "Story Teller": {  
   name: "Story Teller",
   attack: 0,
   defense: 0,
-  type: "",
+  type: "Boss",
   flavorText: "his eyes tell a story"
 },
   "Mr Cameo":{
     name: "Mr Cameo",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Loyal Customer",
     flavorText: "He is everywhere and no where"
   },
   "Grapling Hook": {
     name: "Grapling Hook",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "COME OVER HERE!"
   },
   "Just Robots": {
     name: "Just Robots",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Loyal Customers",
     flavorText: "you think they are for justice?"
   },
   "Brent's Approval!": {
     name: "Brent's Approval!",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "wife beater"
   },
   "Michael's Supremecy": {
     name: "Michael's Supremecy",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "The Deepest V"
   },
   "Tinley's Torcher": {
     name: "Tinley's Torcher",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: ""
   },
   "Bakery Morgage?": {
     name: "Bakery Morgage?",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: "that explosions okay"
+    type: "Gotcha",
+    flavorText: "that explosions okay ,but You gotta pay the mortage cuh",
+    effect(){
+      /* You gotta the mortage cuh   */
+    }
   },
   "Epic Clash": {
     name: "Epic Clash",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "clause",
     flavorText: ""
   },
   "The Bitch": {
     name: "The Bitch",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Boss",
+    flavorText: "",
   },
-  "The Italian Job": {
+  "The Pie-talian Job": {
     name: "The Italian Job",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "we can't"
   },
   "What's in the bag?": {
     name: "What's in the bag?",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "badly strong"
   },
   "Grandmaster Elements": {
     name: "Grandmaster Elements",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Corprate",
     flavorText: "gather elements..neeeeeeeeeeeeeeee"
   },
   "Fireworks": {
     name: "Fireworks",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: ""
   },
   "Magic Cookie": {
     name: "Magic Cookie",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
   "Make it a win": {
     name: "Make it a win",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "or make it a fight"
   },
   "Seal of Piesosalecze": {
     name: "Seal of Piesosalecze",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: ""
   },
   "Enter Darkness": {
     name: "Enter Darkness",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "Only this ... The whole time"
   },
   "MuffenMan": {
     name: "MuffenMan",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Bakery Bunch",
+    flavorText: "i think it's public domain"
   },
   "SmackaDile": {
     name: "SmackaDile",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Hooligan",
     flavorText: ""
   },
   "I'm on my way!": {
     name: "I'm on my way!",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
-  "Legs of Puffin": {
-    name: "Legs of Puffin",
+  "Billy Payes": {
+    name: "Billy Payes",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "corprate",
+    flavorText: "As seen on Pie Per view. Pie, Billy Payes Here"
   },
   "Time for my cameo!": {
     name: "Time for my cameo!",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: ""
   },
   "PieGuy": {
     name: "PieGuy",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Bakery Bunch",
+    flavorText: "KAAAAA WOOOOOW"
   },
   "Health Scrub": {
     name: "Health Scrub",
     attack: 0,
     defense: 0,
     type: "",
-    flavorText: ""
+    flavorText: "The fork is a health scrub??"
   },
   "Army of Angry Dollars": {
     name: "Army of Angry Dollars",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "Hooligans",
+    flavorText: "forever fighting the robots"
   },
   "Pimerization": {
     name: "Pimerization",
     attack: 0,
     defense: 0,
-    type: "",
-    flavorText: ""
+    type: "clause",
+    flavorText: "to be fair, polymerization is chemical reaction of plastics "
   },
   "Robot from the Future": {
     name: "Robot from the Future",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Loyal Customer",
     flavorText: "This piece of crap car"
   },
   "Zack's 10,000 Clauses": {
     name: "Zack's 10,000 Clauses",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: "Here is a list of 10,000 Provisions you must observe"
   },
   "SmackaJack": {
     name: "SmackaJack",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Hooligan",
     flavorText: "This idiot just ghosted to lane"
   },
   "BrainSmorm": {
     name: "BrainSmorm",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Clause",
     flavorText: "It's not like you can fix typos"
   },
   "Shark Tank Full of Nothing but Sharks": {
     name: "Shark Tank Full of Nothing but Sharks",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
   "Super Cookie Dough Bomb": {
     name: "Super Cookie Dough Bomb",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Gotcha",
     flavorText: ""
   },
   "banaman": { 
     name: "banaman",
     attack: 0,
     defense: 0,
-    type: "",
+    type: "Hooligan",
     flavorText: "Everyone likes me"
   },
 };
