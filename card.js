7853ddd33060375ab9973e6d117a2cefc5867714
miaklwalker <ouisihai@gmail.com>
2019-09-12 22:44:14 -0500

    Commiting changes

--- card.js
+++ card.js
@@ -1,15 +1,5 @@
-// Magic Hate Ball
-
-let dice = Math.floor(Math.random()*8)
-let output
-
-if(dice===0){output = "Yes";}
-if (dice === 1) {output = "Hello";}
-if (dice === 2) {output = "Goodbye";}
-if (dice === 3) {output = "Fuck you ";}
-if (dice === 4) {output = "I Said Good Day Sir";}
-if (dice === 5) {output = "Congratulations you played yourself";}
-if (dice === 6) {output = "Poo";}
-if (dice === 7) {output = "Another one";}
-if (dice === 8) {output = "Shiiiiiiiiiiiiit";}
-console.log(output)
+export default class Card{
+    constructor(){
+        this.name
+    }
+}
